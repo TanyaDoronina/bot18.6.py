ddb0acbc422746f685724d04007897c089dcf251
Tanya_Do <127937033+TanyaDoronina@users.noreply.github.com>
2023-08-20 09:42:30 +0300

    bot18.6.py

--- bot18.6.py
+++ bot18.6.py
@@ -1,56 +1,49 @@
 import telebot
-from config import token, keys
-from extensions import ExchangeException, Exchange
+from config import keys, TOKEN
+from extensions import ConvertionException, Exchange
 
 bot = telebot.TeleBot('6064515207:AAFZSocU8756qRbM6Fz11-NyJTsg0EM5fIc')
-currency = CurrencyConverter()
 
 
 @bot.message_handler(commands=['start'])
-def start(messange):
-    bot.send_message(message.chat.id, 'Привет, введите сумму')
-    bot.register_next_step_handler(message, summa)
+def start(message: telebot.types.Message):
+    text = 'Привет! Я Бот-Конвертер валют и я могу:  \n- Показать список доступных валют через команду /values \
+    \n- Вывести конвертацию валюты через команду <имя валюты> <в какую валюту перевести> <количество переводимой валюты>\n \
+- Напомнить, что я могу через команду /help'
+    bot.reply_to(message, text)
 
-def summa(message):
-    global amount
+
+@bot.message_handler(commands=['help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать конвертацию, введите команду боту в следующем формате: \n<имя валюты> <в какую валюту перевести> <количество переводимой валюты>\nЧтобы увидеть список всех доступных валют, введите команду\n/values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key,))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def get_price(message: telebot.types.Message):
     try:
-        amount = int(message.text.strip())
-    except ValueError:
-        bot.send_message(message.chat.id, 'Неверный формат. Впишите сумму ')
-        bot.register_next_step_handler(message, summa)
-        return
-    if amount > 0:
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        btn1 = types.InlineKeyboardButton('USD/EUR', callback_data='usd/eur')
-        btn2 = types.InlineKeyboardButton('EUR/USD', callback_data='eur/usd')
-        btn3 = types.InlineKeyboardButton('USD/RUB', callback_data='usd/rub')
-        btn4 = types.InlineKeyboardButton('Другое значение', callback_data='else')
-        markup.add(btn1, btn2, btn3, btn4)
-        bot.send_message(message.chat.id, 'Выберите пару валют', reply_markup=markup)
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Введите команду или 3 параметра')
+
+        quote, base, amount = values
+        total_base = Exchange.get_price(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Что-то пошло не так с {e}')
     else:
-        bot.send_message(message.chat.id, ' Число долдно быть больше 0. Впищшите сумму')
-        bot.register_next_step_handler(message, summa)
+        text = f'Переводим {quote} в {base}\n{amount} {quote} = {total_base} {base}'
+        bot.send_message(message.chat.id, text)
 
 
-@bot.callback_query_handlers(func=lambda call: True)
-def callback(call):
-    if call.data != 'else':
-      values = call.data.upper().split('/')
-      res = currency.convert(amount, values[0], values[1])
-      bot.send_message(call.message.chat.id, f'Получается: {round(res, 2)}. Можете заново вписать сумму')
-      bot.register_next_step_handler(call.message, summa)
-    else:
-        bot.send_message(call.message.chat.id, 'Введите пару значений через слэш')
-        bot.register_next_step_handler(call.message, my_currency)
-
-def my_currency(message):
-    try:
-        values = message.text.upper().split('/')
-        res = currency.convert(amount, values[0], values[1])
-        bot.send_message(message.chat.id, f'Получается: {round(res, 2)}. Можете заново вписать сумму')
-        bot.register_next_step_handler(message, summa)
-    except Exception:
-        bot.send_message(message.chat.id, f'Что-то не так. Впишите значение заново')
-        bot.register_next_step_handler(message, my_currency)
-
-bot.polling(none_stop=True)
+bot.polling()
